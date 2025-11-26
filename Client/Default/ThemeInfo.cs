a1d3f8b8ba9923413372776408bd372801d8bae3
Sharafudeen <afsharfudeen@gmail.com>
2025-11-26 17:47:20 +0530

    Default theme info updated

--- Client/Default/ThemeInfo.cs
+++ Client/Default/ThemeInfo.cs
@@ -9,7 +9,7 @@ namespace StudioElf.Theme.Bootswatch.Default
         public Oqtane.Models.Theme Theme => new Oqtane.Models.Theme
         {
             Name = "Bootswatch Default",
-            Version = "6.5.3.8",
+            Version = "10.5.3.8",
             ThemeSettingsType = "StudioElf.Theme.Bootswatch.ThemeSettings, StudioElf.Theme.Bootswatch.Oqtane",
             ContainerSettingsType = "StudioElf.Theme.Bootswatch.ContainerSettings, StudioElf.Theme.Bootswatch.Oqtane",
             PackageName = "StudioElf.Theme.Bootswatch",
@@ -17,6 +17,20 @@ namespace StudioElf.Theme.Bootswatch.Default
             {
 		        // obtained from https://cdnjs.com/libraries
                 new Stylesheet("https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css", "sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==", "anonymous"),
+
+                new Stylesheet("Themes/AyroUI/assets/css/glightbox.min.css"),
+                new Stylesheet("Themes/AyroUI/assets/css/lineicons.css"),
+                new Stylesheet("Themes/AyroUI/assets/css/loading-bar.min.css"),
+                new Stylesheet("Themes/AyroUI/assets/css/tiny-slider.css"),
+
+                new Stylesheet("Themes/AyroUI/assets/scss/starter.css"),
+                new Stylesheet("Themes/AyroUI/assets/scss/tabs-accordions/tabs.css"),
+
+                new Script("Themes/AyroUI/assets/js/count-up.min.js"),
+                new Script("Themes/AyroUI/assets/js/glightbox.min.js"),
+                new Script("Themes/AyroUI/assets/js/loading-bar.min.js"),
+                new Script("Themes/AyroUI/assets/js/tiny-slider.js"),
+
                 new Stylesheet("Themes/StudioElf.Theme.Bootswatch/Theme.css"),
                 new Stylesheet("Themes/StudioElf.Theme.Bootswatch/Default.css"),
                 new Script("https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.js", "sha512-7Pi/otdlbbCR+LnW+F7PwFcSDJOuUJB3OxtEHbg4vSMvzvJjde4Po1v4BR9Gdc9aXNUNFVUY+SK51wWT8WF0Gg==", "anonymous")
